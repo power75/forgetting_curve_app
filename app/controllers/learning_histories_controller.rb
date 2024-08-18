4fd6d5ecce19ca724bbb99a75827596596529c99
power75 <karurangod@outlook.jp>
2024-08-18 19:59:44 +0900

    controllerの修正

--- app/controllers/learning_histories_controller.rb
+++ app/controllers/learning_histories_controller.rb
@@ -47,13 +47,13 @@ class LearningHistoriesController < ApplicationController
   end
 
   def complete # 学習完了にするボタン
-    @learning_history = LearningHistory.find(params[:id])
+    @learning_history = current_user.learning_histories.find(params[:id])
     @learning_history.update(status: 1)
     redirect_to learning_histories_path, success: '完了にしました！'
   end
 
   def incomplete # 学習未完了に戻すボタン
-    @learning_history = LearningHistory.find(params[:id])
+    @learning_history = current_user.learning_histories.find(params[:id])
     @learning_history.update(status: 0)
     redirect_to learning_histories_path, success: '未完了に戻しました！'
   end
