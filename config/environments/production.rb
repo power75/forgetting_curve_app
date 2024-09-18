bdc1f7ce1a10e74f4d754b1eefc54a6cf73d1bba
power75 <karurangod@outlook.jp>
2024-09-18 21:12:17 +0900

    細かな改訂

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,11 +91,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  #deviseが認証用のURLなどを生成するのに必要になる（らしい）
   config.action_mailer.default_url_options = {  :host => 'https://forgetting-curve-app-1.onrender.com' }
-  #送信方法を指定（この他に:sendmail/:file/:testなどがあります)
   config.action_mailer.delivery_method = :smtp
-  #送信方法として:smtpを指定した場合は、このconfigを使って送信詳細の設定を行います
   config.action_mailer.smtp_settings = {
     #gmail利用時はaddress,domain,portは下記で固定
     address:"smtp.gmail.com",
