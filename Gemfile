7848e7969346ec9668a03a6c59646b9e1f98c6fe
power75 <karurangod@outlook.jp>
2024-10-06 21:15:56 +0900

    お問い合わせフォーム追加

--- Gemfile
+++ Gemfile
@@ -70,6 +70,8 @@ gem 'whenever', require: false
 
 gem 'rubocop', require: false
 
+gem 'dotenv-rails'
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem 'debug', platforms: %i[mri mingw x64_mingw]
--- config/routes.rb
+++ config/routes.rb
@@ -23,6 +23,7 @@ Rails.application.routes.draw do
     end
   end
   resources :notifications, only: [:index]
+  resources :contacts, only: [:new, :create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root 'static_pages#top'
   # Defines the root path route ("/")
