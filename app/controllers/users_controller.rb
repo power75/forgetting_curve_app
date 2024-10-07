52aefc8f578d192ceaa4251736367b2cf1a55955
power75 <karurangod@outlook.jp>
2024-10-07 20:41:46 +0900

    ユーザー登録後のオートログイン

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,6 +23,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      auto_login(@user)
       redirect_to(root_path, success: 'ユーザー登録に成功しました')
     else
       flash.now[:danger] = 'ユーザー登録に失敗しました'
