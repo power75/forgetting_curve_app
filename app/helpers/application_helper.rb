174a32efb723d3cab1c3cdca0e935c29082cbea1
power75 <karurangod@outlook.jp>
2025-01-22 23:11:25 +0900

    静的OGP(X)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,28 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def default_meta_tags
+    {
+      site: 'Wisdom Whistle',
+      title: 'Wisdom Whistle',
+      reverse: true,
+      charset: 'utf-8',
+      description: '復習を通知するサービスです',
+      canonical: request.original_url,
+      og: {
+        site_name: 'Wisdom Whistle',
+        title: 'Wisdom Whistle',
+        description: '復習を通知するサービスです',
+        type: 'website',
+        url: request.original_url,
+        image: image_url('ogp_image.png'),
+        local: 'ja-JP'
+      },
+      twitter: {
+        card: 'summary_large_image',
+        site: '@',
+        image: image_url('ogp_image.png')
+      }
+    }
+  end
 end
